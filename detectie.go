820ed1df7b7886a05b12d1aa799a78ec1f87c841
zc310 <liushiyan@msn.com>
2019-03-22 09:45:01 +0800

    speed up

--- detectie.go
+++ detectie.go
@@ -8,37 +8,39 @@ import (
 
 // DetectIe returns version of IE
 func DetectIe(ua string) (bool, float32) {
-	var err error
-	var ver float64
-	re := regexp.MustCompile(`MSIE (\d+.\d+)`)
-	a := re.FindStringSubmatch(ua)
-	if len(a) == 2 {
-		ver, err = strconv.ParseFloat(a[1], 32)
-		if err != nil {
-			return true, 0
-		}
-		return true, float32(ver)
-	}
-	if strings.Index(ua, "Trident/") > 0 {
-		re = regexp.MustCompile(`rv:(\d+.\d+)`)
-		a = re.FindStringSubmatch(ua)
-		if len(a) == 2 {
-			ver, err = strconv.ParseFloat(a[1], 32)
-			if err != nil {
+	var (
+		err error
+		ver float64
+		re  *regexp.Regexp
+		a   []string
+		n   int
+	)
+	if n = strings.Index(ua, "MSIE "); n > 0 {
+		re = regexp.MustCompile(`MSIE (\d+.\d+)`)
+		if a = re.FindStringSubmatch(ua); len(a) == 2 {
+			if ver, err = strconv.ParseFloat(a[1], 32); err != nil {
 				return true, 0
 			}
 			return true, float32(ver)
 		}
 	}
-
-	re = regexp.MustCompile(`Edge/(\d+.\d+)`)
-	a = re.FindStringSubmatch(ua)
-	if len(a) == 2 {
-		ver, err = strconv.ParseFloat(a[1], 32)
-		if err != nil {
-			return true, 0
+	if n = strings.Index(ua, "Trident/"); n > 0 {
+		re = regexp.MustCompile(`rv:(\d+.\d+)`)
+		if a = re.FindStringSubmatch(ua); len(a) == 2 {
+			if ver, err = strconv.ParseFloat(a[1], 32); err != nil {
+				return true, 0
+			}
+			return true, float32(ver)
 		}
-		return true, float32(ver)
+	}
+	if n = strings.Index(ua, "Edge/"); n > 0 {
+		re = regexp.MustCompile(`Edge/(\d+.\d+)`)
+		if a = re.FindStringSubmatch(ua); len(a) == 2 {
+			if ver, err = strconv.ParseFloat(a[1], 32); err != nil {
+				return true, 0
+			}
+			return true, float32(ver)
+		}
 	}
 
 	return false, 0
